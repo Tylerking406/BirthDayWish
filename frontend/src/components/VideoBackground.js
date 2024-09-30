c6036dd388f45d21792eb7b4207f754343f60809
Arinao Ndou <tylerking406@gmail.com>
2024-09-30 10:52:42 +0200

    video bg is using local storage for optimization

--- frontend/src/components/VideoBackground.js
+++ frontend/src/components/VideoBackground.js
@@ -1,36 +1,17 @@
 // VideoBackground.js
 import { Box } from '@mui/material';
-import { getDownloadURL, ref } from 'firebase/storage';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import MultiVideoBackground from '../components/MultiVideoBackground.jsx';
-import { storage } from '../firebase.js'; // Make sure to import the Firebase storage
 
 const VideoBackground = ({ children }) => {
-  const [videoURLs, setVideoURLs] = useState([]); // State to hold the video URLs
-
-  useEffect(() => {
-    const fetchVideos = async () => {
-      try {
-        // References to your videos in Firebase Storage
-        const videoRefs = [
-          ref(storage, 'videos/us.mp4'),
-          ref(storage, 'videos/friends.mp4'),
-        ];
-
-        // Fetch download URLs for each video
-        const urls = await Promise.all(videoRefs.map((videoRef) => getDownloadURL(videoRef)));
-        setVideoURLs(urls); // Update state with fetched URLs
-      } catch (error) {
-        console.error('Error fetching video URLs:', error);
-      }
-    };
-
-    fetchVideos(); // Fetch the video URLs when the component mounts
-  }, []);
+  const videos = [
+    '/assets/videos/us.mp4',
+    '/assets/videos/friends.mp4'
+  ];
 
   return (
-    <MultiVideoBackground
-      videos={videoURLs} // Use the dynamically fetched video URLs
+    <MultiVideoBackground 
+      videos={videos}
       objectFit="cover"
       objectPosition="center center"
     >
