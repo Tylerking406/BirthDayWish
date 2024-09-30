89465a47af69086a6a0723b15eeb6bae01615f40
Arinao Ndou <tylerking406@gmail.com>
2024-09-30 02:08:36 +0200

    material-ui icons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
-import './App.css';
+import Button from '@mui/material/Button';
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      Happy
-    </div>
-  );
+    return (
+        <div>
+            <Button variant="contained" color="primary">
+                Hello World
+            </Button>
+        </div>
+    );
 }
 
 export default App;
